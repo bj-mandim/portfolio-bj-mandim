3f331c94c36c0517148edef79e080125b64e872c
bj-mandim <beatrizjuliomandim@gmail.com>
2023-03-05 22:30:03 -0300

    feat: final details links

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,7 +30,7 @@ import userIMG from "../../public/static/img/logo/user.png";
 
 export const Home = (): JSX.Element => {
   const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
+  const portfolioUrl = `https://github.com/${userData.githubUser}/portfolio-bj-mandim`;
 
   return (
     <main id="home">
